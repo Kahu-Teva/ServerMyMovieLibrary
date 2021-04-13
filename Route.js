558beaa6dbc87cc189a37b226fccdfb77eb59e72
Kahu-Teva <tevaa.kahu@gmail.com>
2021-04-12 16:47:06 -1000

    test

--- Route.js
+++ Route.js
@@ -1,5 +1,30 @@
 const express = require("express");
+const MongoClient = require('mongodb').MongoClient;
+
 const functions = require("./functions.js");
+
+const connectionString = "mongodb://127.0.0.1:27017";
+
+MongoClient.connect(connectionString, (err, client) => {
+    if (err) return console.error(err)
+    console.log('Connected to Database');
+    const db = client.db('mylibrarymovie');
+
+    /*
+    db.collection("movies").findOne({}, function(err, result) {
+        if (err) throw err;
+        console.log("Movies : " + result.title);
+        client.close();
+    });
+    */
+
+    db.collection("movies").find({}).toArray(function(err, result) {
+        if (err) throw err;
+        console.log("Movies : " + result[3].title);
+        client.close();
+    });
+});
+
 router = express.Router();
 
 router.get("/movies", functions.getMovies);
